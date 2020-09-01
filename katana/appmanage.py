c18034a1b872f7eb14a6289ff79be75418f0627d
wf-docs <jyoti.gupta@terralogic.com>
2020-09-01 16:51:30 +0530

    Fix given for conatiner env

--- katana/appmanage.py
+++ katana/appmanage.py
@@ -55,11 +55,14 @@ native_dir = BASE_DIR + "/native"
 settings_file = BASE_DIR + "/wui/settings.py"
 
 if os.environ["pipmode"] == "True":
+
     virtual_env = os.getenv('VIRTUAL_ENV')
     if virtual_env:
         app_config_file = virtual_env + os.sep + "katana_configs" + os.sep + "app_config.json"
     else:
         app_config_file = site.getuserbase() + os.sep + "katana_configs" + os.sep + "app_config.json"
+        if not os.path.exists(app_config_file):
+            app_config_file = "/usr/local/katana_configs/app_config.json"
 else:
     app_config_file = os.path.join(BASE_DIR, "katana_configs", "app_config.json")
 
