feaac13332208d0e4895e08995286f638a92d8ce
Kanakatte, Manjunath <manjunath.kanakatte@fujitsu.com>
2020-11-19 08:29:19 -0600

    added release files and fixed path issue

--- katana/wui/core/templatetags/core_filters.py
+++ katana/wui/core/templatetags/core_filters.py
@@ -9,6 +9,19 @@ BASE_DIR = nav_obj.get_katana_dir()
 
 register = template.Library()
 
+if os.environ["pipmode"] == "True":
+    virtual_env = os.getenv('VIRTUAL_ENV')
+    if virtual_env:
+        app_config_file = virtual_env + os.sep + "katana_configs" + os.sep + "app_config.json"
+    elif os.path.exists(site.getuserbase() + os.sep + "katana_configs"):
+        app_config_file = site.getuserbase() + os.sep + "katana_configs" + os.sep + "app_config.json"
+    elif os.path.exists("/usr/local/katana_configs"):
+        app_config_file = "/usr/local/katana_configs/app_config.json"
+    else:
+        print("--An error occured: Can not find katana_configs directory")
+        exit()
+else:
+    app_config_file = os.path.join(BASE_DIR, "katana_configs", "app_config.json")
 
 @register.filter(name="get_app_name", is_safe=True)
 def get_app_name(value):
@@ -20,7 +33,6 @@ def get_app_name(value):
 
 @register.filter(name="check_timer_flag", is_safe=True)
 def check_timer_flag(value):
-    app_config_file = os.path.join(BASE_DIR, "katana_configs/app_config.json")
     data = read_json_data(app_config_file)
     Flag = data["enable_utc_clock"].lower()
     return Flag
@@ -28,7 +40,6 @@ def check_timer_flag(value):
 
 @register.filter(name="check_fujitsu_logo_flag", is_safe=True)
 def check_fujitsu_logo_flag(value):
-    app_config_file = os.path.join(BASE_DIR, "katana_configs/app_config.json")
     data = read_json_data(app_config_file)
     Flag = data["enable_fujitsu_logo"].lower()
     return Flag
